a227d277a473846753e40507fee5d2013829fd0e
Veronica <kqadb@mail.ru>
2022-03-31 16:19:29 +0300

    Initial commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,52 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code her
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+}
